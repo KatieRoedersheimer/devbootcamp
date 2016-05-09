3f8298c8fb5eabbf712868728e4a4b7929780e7c
Katie <KatieRoedersheimer@users.noreply.github.com>
2016-05-09 13:26:21 -0700

    assignment7
    created a clock that updates the month, day, year, and by minutes, second written in JavaScript, HTML5, and CSS3

---
+++ assignment7/js/date.js
@@ -0,0 +1,22 @@
+function myDate() {
+var date = new Date();
+var ampm;
+var hour =  date.getHours();
+var minutes = date.getMinutes();
+var month = date.getMonth();
+var fullMonth = ["January", "February", "March", "April", "May", "June", "July"];
+month = fullMonth[month];
+var day = date.getDate();
+var year = date.getFullYear();
+if(hour>12){
+	hour=hour-12;
+	ampm = "PM"
+}else{
+	ampm ="AM"
+}
+if(minutes < 10){
+	minutes = "0" + minutes
+}
+document.getElementById("date").innerHTML = hour +":" + minutes + ":" + ampm + " " + month + " " + day + " " + year;
+}
+setInterval(myDate, 1000);
